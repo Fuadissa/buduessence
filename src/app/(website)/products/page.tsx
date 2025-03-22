d8a0387a10d3b1944c42747fad78a4ee49c35c8e
Fuad <issafuad1818@gmail.com>
2025-03-22 13:14:21 -0700

    Refactor product page component to use default export and improve readability

--- src/app/(website)/products/page.tsx
+++ src/app/(website)/products/page.tsx
@@ -1,5 +1,9 @@
-import React from "react";
+import React from 'react'
 
-export const page = () => {
-  return <div>page</div>;
-};
+const ProductPage = () => {
+  return (
+    <div>ProductPage</div>
+  )
+}
+
+export default ProductPage
